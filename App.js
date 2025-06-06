0909b0bf3448bb31774a5afec1a165c5e129659d
HarshShukla01 <hardyharshshukla@gmail.com>
2025-06-07 01:44:39 +0530

    Updated App

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 // const heading = React.createElement("h1", {}, "Hello World! from React");
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
@@ -19,7 +22,7 @@
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h1", {}, "I am h1 tag of child1"),
     React.createElement("h2", {}, "I am h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
