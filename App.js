b4aeb3e46b1b26cfde26f9e1c0beb758f23f607d
HarshShukla01 <hardyharshshukla@gmail.com>
2025-06-08 01:39:08 +0530

    Modifications after EP03

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement("h1", {}, "Hello World! from React");
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
+// Created React Element using React
+const heading = React.createElement("h1", {}, "Hello World! from React");
+
+// Create React Element using JSX
+const jsxHeading = <h1 id="heading">Hello React using JSX!</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
 
 /**
  * <div id="parent">
@@ -17,18 +22,58 @@ import ReactDOM from 'react-dom/client';
  *      </div>
  * </div>
  *
- * ReactElement(object) => HTML(Browser understands)
+ * ReactElement(object) => after rendering it becomes => HTMLelement(Browser understands)
  */
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1 tag of child1"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
-  ]),
-]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I am h1 tag of child1"),
+//     React.createElement("h2", {}, "I am h2 tag"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "I am h1 tag"),
+//     React.createElement("h2", {}, "I am h2 tag"),
+//   ]),
+// ]);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+
+// React Components
+// React Functional Component
+const HeadingComponent = () => {
+  return <h1 className="heading">React Functional Component</h1>;
+};
+
+const Title = () => (
+  <div>
+    <h1 className="head" tabIndex="1">
+      React Learning
+    </h1>
+  </div>
+);
+
+const elem = (
+  <h4>elem</h4>
+);
+
+const element = (
+  <div>
+    <Title/>
+    <Title></Title>
+    {Title()}
+    {elem}
+    <h3>element</h3>
+  </div>
+);
+
+// Component Composition - using component within component
+const HeadingComponent2 = () => (
+  <div id="container">
+    <Title />
+    {100 + 200}
+    {element}
+    <h1 className="heading">React Functional Component</h1>
+  </div>
+);
+
+root.render(<HeadingComponent2 />);
